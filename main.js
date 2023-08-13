fe0a4ab4c10893b2681eebced829b9570cacff41
b4n1y4 <riteshgupta7645@gmail.com>
2023-08-13 14:39:01 +0530

    changes
    made responsive
    added favicon
    added ticking sound

--- main.js
+++ main.js
@@ -13,8 +13,6 @@ const s_hand = document.getElementById("s");
 
 const run_time = () => {
   clearTimeout();
-  console.log(h, m, s);
-
   if (s >= 60) {
     m += 1;
     s = 0;
@@ -23,17 +21,31 @@ const run_time = () => {
     h += 1;
     m = 0;
   }
-  s += 1;
   h_hand.style.transform = `rotate(${
     h * (360.0 / 12) + m * (360.0 / 12 / 60) + s * (360.0 / 12 / 60 / 60)
   }deg)`;
   m_hand.style.transform = `rotate(${
     m * (360.0 / 60) + s * (360.0 / 60 / 60)
   }deg)`;
+  s += 1;
   s_hand.style.transform = `rotate(${s * (360.0 / 60)}deg)`;
-  setTimeout(() => {
-    run_time();
-  }, 1000);
 };
 
-run_time();
+setInterval(() => {
+  run_time();
+  new Audio("./tick.mp3").play();
+}, 1000);
+
+// theme toggle
+
+// let isDay = false;
+
+// const btn = document.getElementById("btn");
+// btn.onclick = () => {
+//   if (isDay) {
+//     btn.classList.remove("day");
+//   } else {
+//     btn.classList.add("day");
+//   }
+//   isDay = !isDay;
+// };
